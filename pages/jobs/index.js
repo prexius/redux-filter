d22d3c0a1310ea2e306c90af1e9b8d628439265e
imsaifun <imsaifun@gmail.com>
2023-06-03 11:13:24 +0400

    working

--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 // import { useGetFilterJobsQuery } from '@redux/features/jobs/jobsApi'
 export default function Jobs() {
     
-    const [industry, setindustry] = useState("software")
+    // const [industry, setindustry] = useState("software")
     // const {data} = useGetFilterJobsQuery({industry})
   return (
     <>
